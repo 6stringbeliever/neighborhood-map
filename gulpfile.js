391910543fcf263b74cc65a85ac368d2f2cd4f48
Shaun Kelly <shaunkelly@gmail.com>
2015-01-25 15:58:39 -0500

    Update gulpfile to watch status and move lib files

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('sass', function () {
 
 // Minify JavaScript
 gulp.task('minifyjs', function() {
-  return gulp.src('dev/**/*.js')
+  return gulp.src('dev/js/*.js')
              .pipe(uglify())
              .pipe(gulp.dest('dist'));
 });
@@ -40,5 +40,32 @@ gulp.task('moveimages', function() {
              .pipe(gulp.dest('dist/img/'));
 });
 
+// Move lib css files; don't need to do this every time
+gulp.task('movelibcss', function() {
+  return gulp.src('dev/css/lib/*.css')
+             .pipe(gulp.dest('dist/css/lib/'));
+});
+
+// Move lib js files; don't need to do this every time
+gulp.task('movelibjs', function() {
+  return gulp.src('dev/js/lib/*.js')
+  .pipe(gulp.dest('dist/js/lib/'));
+});
+
+// Move all lib files
+gulp.task('movelib', ['movelibcss', 'movelibjs']);
+
 // Do everything by default
 gulp.task('default', ['lint', 'sass', 'minifyhtml', 'minifyjs', 'moveimages']);
+
+// Watch HTML, Sass, JavaScript files and update on change
+gulp.task('watch', function() {
+  // Watch Sass files and update
+  gulp.watch('dev/css/*.scss', ['sass']);
+  
+  // Watch HTML files and minify output
+  gulp.watch('dev/*.html', ['minifyhtml']);
+  
+  // Watch js files and lint and minify output
+  gulp.watch('dev/js/*.js', ['lint', 'minifyjs']);
+});
