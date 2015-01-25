54769630a9f27515f883971be5b33c0541b30a5a
Shaun Kelly <shaunkelly@gmail.com>
2015-01-25 12:19:09 -0500

    Set up basic gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,44 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var minifyInline = require('gulp-minify-inline');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Lint JavaScript
+gulp.task('lint', function() {
+    return gulp.src('dev/js/*.js')
+               .pipe(jshint())
+               .pipe(jshint.reporter('default'));
 });
+
+// Minify HTML and inline scripts and CSS
+gulp.task('minifyhtml', function() {
+  return gulp.src('dev/**/*.html')
+             .pipe(minifyHTML())
+             .pipe(minifyInline())
+             .pipe(gulp.dest('dist'));
+});
+
+// Compile Sass
+gulp.task('sass', function () {
+  return gulp.src('dev/css/*.scss')
+             .pipe(sass({outputStyle: 'compressed'}))
+             .pipe(gulp.dest('dist/css'));
+});
+
+// Minify JavaScript
+gulp.task('minifyjs', function() {
+  return gulp.src('dev/**/*.js')
+             .pipe(uglify())
+             .pipe(gulp.dest('dist'));
+});
+
+// Move images with PNG or JPG extension
+gulp.task('moveimages', function() {
+  return gulp.src('dev/img/**/*.+(png|jpg)')
+             .pipe(gulp.dest('dist/img/'));
+});
+
+// Do everything by default
+gulp.task('default', ['lint', 'sass', 'minifyhtml', 'minifyjs', 'moveimages']);
