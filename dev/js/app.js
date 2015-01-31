3dcb8001cabbf795df2529f144796b2cbfbc23f7
Shaun Kelly <shaunkelly@gmail.com>
2015-01-31 11:00:56 -0500

    Store map in ViewModel; move add markers to update in googlemaps binding

--- dev/js/app.js
+++ dev/js/app.js
@@ -24,7 +24,7 @@ $(function() {
       return {
                position: this.mapPoint(),
                title: this.name()
-             }
+             };
     }, this);
   };
   
@@ -48,6 +48,8 @@ $(function() {
   var ViewModel = function() {
     var self = this;
     
+    this.map = null;
+    
     this.stadiums = ko.observableArray([]);
     for (var stadium in stadiumData) {
       this.stadiums.push(new Stadium(stadiumData[stadium]));
@@ -55,8 +57,11 @@ $(function() {
   };
   
   ko.bindingHandlers.googlemap = {
-    init: function(element, valueAccessor) {
-      var value = valueAccessor;
+    /*
+      On init, create the map and attach the map controls.
+    */
+    init: function(element, valueAccessor, allBindings, 
+                    viewModel, bindingContext) {
       var mapOptions = {
         zoom: 4,
         center: { lat: 39.8282, lng: -98.5795 },
@@ -67,29 +72,37 @@ $(function() {
             position: google.maps.ControlPosition.RIGHT_BOTTOM
         },
       };
-      var map = new google.maps.Map(element, mapOptions);
       
-      for (var i in value().stadiums()) {
-        var stadium = value().stadiums()[i];
-        if (stadium.visible()) {
-          var marker = new google.maps.Marker(stadium.marker());
-          marker.setMap(map);
-        }
-
-      }
-            
+      bindingContext.$data.map = new google.maps.Map(element, mapOptions);
+                  
       /*
         When the tiles are loaded, we detach all the KO views and reattach
         them as map controls. This way we get all the goodness of map controls
         but don't have to worry about when to ko.applyBinding.
       */
-      google.maps.event.addListener(map, 'tilesloaded', function(evt) {
+      google.maps.event.addListener(bindingContext.$data.map, 'tilesloaded', function(evt) {
         var control = document.createElement('div');
         control.id = 'stadium-list-control';
         var list = $('#stadium-list').detach();
-        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(control);
-        list.appendTo('#stadium-list-control')
+        bindingContext.$data.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(control);
+        list.appendTo('#stadium-list-control');
       });
+    },
+    /*
+      On update, cycle through the stadium list passed through valueAccessor and
+      display all markers set to visible.
+    */
+    update: function(element, valueAccessor, allBindings, 
+                      viewModel, bindingContext) {
+      var value = valueAccessor;
+
+      for (var i in value().stadiums()) {
+        var stadium = value().stadiums()[i];
+        if (stadium.visible()) {
+          var marker = new google.maps.Marker(stadium.marker());
+          marker.setMap(bindingContext.$data.map);
+        }
+      }
     }
   };
   
