8d6c74c53650a2eb0f25fa469a13e4bbdcf3a642
Shaun Kelly <shaunkelly@gmail.com>
2015-02-03 20:13:02 -0500

    Add team info to stadium data and infowindow; move listener functions to closure in googlemap binding

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,22 +1,32 @@
 $(function() {
-  
+
   var Stadium = function(data) {
     this.name = ko.observable(data.name);
     this.lat = ko.observable(data.lat);
     this.lng = ko.observable(data.lng);
-    this.visible = typeof data.visible === 'boolean' ? 
+    /* Default visible to true unless it's set in the passed in data */
+    this.visible = typeof data.visible === 'boolean' ?
                     ko.observable(data.visible) : ko.observable(true);
-    /* 
+
+    this.teams = ko.observableArray([]);
+
+    for (var i = 0; i < data.teams.length; i++) {
+      this.teams.push({'name': ko.observable(data.teams[i].name),
+                       'sport': ko.observable(data.teams[i].sport),
+                       'league': ko.observable(data.teams[i].league)});
+    }
+
+    /*
        Returns the location in a format that's useful for
        Google Maps.
      */
     this.mapPoint = ko.computed(function() {
-      return { 
-              'lat': this.lat(), 
-              'lng': this.lng() 
+      return {
+              'lat': this.lat(),
+              'lng': this.lng()
              };
     }, this);
-    
+
     /*
        Returns a Google maps marker for the stadium.
     */
@@ -25,42 +35,57 @@ $(function() {
                title: this.name()
              }));
   };
-  
+
   var stadiumData = [
     { 'name': 'Turner Field',
       'lat':  33.735278,
-      'lng': -84.389444
+      'lng': -84.389444,
+      'teams': [{ 'name': 'Atlanta Braves',
+                  'sport': 'baseball',
+                  'league': 'mlb' }]
     },
     {
       'name': 'Target Field',
-      'lat': 44.981667, 
-      'lng': -93.278333
+      'lat': 44.981667,
+      'lng': -93.278333,
+      'teams': [{ 'name': 'Minnesota Twins',
+                  'sport': 'baseball',
+                  'league': 'mlb' }]
     },
     {
       'name': 'O.co Coliseum',
       'lat': 37.751667,
-      'lng': -122.200556
+      'lng': -122.200556,
+      'teams': [{ 'name': 'Oakland Athletics',
+                  'sport': 'baseball',
+                  'league': 'mlb' },
+                { 'name': 'Oakland Raiders',
+                  'sport': 'football',
+                  'league': 'nfl'}]
     }
   ];
-  
+
   var ViewModel = function() {
     var self = this;
-    
+
     this.map = null;
-    
+
+    this.infowindow = new google.maps.InfoWindow(
+        { content: '<div id="info-window"></div>' });
+
     this.stadiums = ko.observableArray([]);
     for (var stadium in stadiumData) {
       this.stadiums.push(new Stadium(stadiumData[stadium]));
     }
-    
-    this.selectedStadium = null;
+
+    this.selectedStadium = ko.observable(null);
   };
-  
+
   ko.bindingHandlers.googlemap = {
     /*
       On init, create the map and attach the map controls.
     */
-    init: function(element, valueAccessor, allBindings, 
+    init: function(element, valueAccessor, allBindings,
                     viewModel, bindingContext) {
       var mapOptions = {
         zoom: 4,
@@ -72,40 +97,62 @@ $(function() {
             position: google.maps.ControlPosition.RIGHT_BOTTOM
         },
       };
-      
-      bindingContext.$data.map = new google.maps.Map(element, mapOptions);
-                  
+      var ctx = bindingContext;
+
+      ctx.$data.map = new google.maps.Map(element, mapOptions);
+
       /*
         When the tiles are loaded, we detach all the KO views and reattach
         them as map controls. This way we get all the goodness of map controls
         but don't have to worry about when to ko.applyBinding.
       */
-      google.maps.event.addListener(bindingContext.$data.map, 'tilesloaded', function(evt) {
+      google.maps.event.addListener(ctx.$data.map, 'tilesloaded', function(e) {
         var control = document.createElement('div');
         control.id = 'stadium-list-control';
         var list = $('#stadium-list').detach();
-        bindingContext.$data.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(control);
+        ctx.$data.map.controls[google.maps.ControlPosition.TOP_RIGHT]
+                                                              .push(control);
         list.appendTo('#stadium-list-control');
       });
     },
+
     /*
       On update, cycle through the stadium list passed through valueAccessor and
       display all markers set to visible.
     */
-    update: function(element, valueAccessor, allBindings, 
+    update: function(element, valueAccessor, allBindings,
                       viewModel, bindingContext) {
       var value = valueAccessor;
+      var ctx = bindingContext;
 
       for (var i in value().stadiums()) {
         var stadium = value().stadiums()[i];
         if (stadium.visible()) {
-          stadium.marker().setMap(bindingContext.$data.map);
+          stadium.marker().setMap(ctx.$data.map);
+          addClickListener(stadium.marker(), stadium, ctx);
         } else {
           stadium.marker().setMap(null);
         }
       }
+
+      function addClickListener(marker, data, bindingContext) {
+        google.maps.event.addListener(marker, 'click', function() {
+          var infowindow = bindingContext.$data.infowindow;
+          infowindow.close();
+          bindingContext.$data.selectedStadium(data);
+          infowindow.open(bindingContext.$data.map,marker);
+          addDOMListener(infowindow);
+        });
+      }
+
+      function addDOMListener(infowindow) {
+        google.maps.event.addListener(infowindow, 'domready', function() {
+          var windowcontent = $('#selected-stadium-info').html();
+          $('#info-window').html(windowcontent);
+        });
+      }
     }
   };
-  
+
   ko.applyBindings(new ViewModel());
 });
