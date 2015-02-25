dd15c9c3b2e96c5d5b735739cd8915f65453d7ab
Shaun Kelly <shaunkelly@gmail.com>
2015-02-24 22:57:27 -0500

    Move stadium data to separate JS file

---
+++ dev/js/data.js
@@ -0,0 +1,86 @@
+var stadiumData = [
+  { 'name': 'Turner Field',
+    'lat':  33.735278,
+    'lng': -84.389444,
+    'teams': [{ 'name': 'Atlanta Braves',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Target Field',
+    'lat': 44.981667,
+    'lng': -93.278333,
+    'teams': [{ 'name': 'Minnesota Twins',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'O.co Coliseum',
+    'lat': 37.751667,
+    'lng': -122.200556,
+    'teams': [{ 'name': 'Oakland Athletics',
+                'league': 'MLB' },
+              { 'name': 'Oakland Raiders',
+                'league': 'NFL'}]
+  },
+  {
+    'name': 'Bridgestone Arena',
+    'lat': 36.159167,
+    'lng': -86.778611,
+    'teams': [{ 'name': 'Nashville Predators',
+                'league': 'NHL' }]
+  },
+  {
+    'name': 'American Airlines Center',
+    'lat': 32.790556,
+    'lng': -96.810278,
+    'teams': [{ 'name': 'Dallas Stars',
+                'league': 'NHL' },
+              { 'name': 'Dallas Mavericks',
+                'league': 'NBA' }]
+  },
+  {
+    'name': 'Columbus Crew Stadium',
+    'lat': 40.009444,
+    'lng': -82.991111,
+    'teams': [{ 'name': 'Columbus Crew',
+                'league': 'MLS' }]
+  },
+  {
+    'name': 'MTS Center',
+    'lat': 49.892778,
+    'lng': -97.143611,
+    'teams': [{ 'name': 'Winnipeg Jets',
+                'league': 'NHL' }]
+  },
+  {
+    'name': 'Pepsi Center',
+    'lat': 39.748611,
+    'lng': -105.0075,
+    'teams': [{ 'name': 'Colorado Avalanche',
+                'league': 'NHL' },
+              { 'name': 'Denver Nuggets',
+                'league': 'NBA'}]
+  },
+  {
+    'name': 'Scottrade Center',
+    'lat': 38.626667,
+    'lng': -90.2025,
+    'teams': [{ 'name': 'St. Louis Blues',
+                'league': 'NHL' }]
+  },
+  {
+    'name': 'United Center',
+    'lat': 41.880556,
+    'lng': -87.674167,
+    'teams': [{ 'name': 'Chicago Blackhawks',
+                'league': 'NHL' },
+              { 'name': 'Chicago Bulls',
+                'league': 'NBA'}]
+  },
+  {
+    'name': 'Xcel Energy Center',
+    'lat': 44.944722,
+    'lng': -93.101111,
+    'teams': [{ 'name': 'Minnesota Wild',
+                'league': 'NHL' }]
+  }
+]; // stadiumData
