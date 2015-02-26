7f7328fdb9fd782fa18d445819e813fca42d96d2
Shaun Kelly <shaunkelly@gmail.com>
2015-02-25 22:08:56 -0500

    Finish adding MLB stadiums

--- dev/js/data.js
+++ dev/js/data.js
@@ -82,5 +82,196 @@ var stadiumData = [
     'lng': -93.101111,
     'teams': [{ 'name': 'Minnesota Wild',
                 'league': 'NHL' }]
+  },
+  {
+    'name': 'Fenway Park',
+    'lat': 42.346389,
+    'lng': -71.0975,
+    'teams': [{ 'name': 'Boston Red Sox',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Oriole Park at Camden Yards',
+    'lat': 39.283889,
+    'lng': -76.621667,
+    'teams': [{ 'name': 'Baltimore Orioles',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Rogers Centre',
+    'lat': 43.641389,
+    'lng': -79.389167,
+    'teams': [{ 'name': 'Toronto Blue Jays',
+                'league': 'MLB' },
+              { 'name': 'Toronto FC',
+                'league': 'MLS' }]
+  },
+  {
+    'name': 'Tropicana Field',
+    'lat': 27.768333,
+    'lng': -82.653333,
+    'teams': [{ 'name': 'Tampa Bay Rays',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Yankee Stadium',
+    'lat': 40.829167,
+    'lng': -73.926389,
+    'teams': [{ 'name': 'New York Yankees',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Comerica Park',
+    'lat': 42.339167,
+    'lng': -83.048611,
+    'teams': [{ 'name': 'Detroit Tigers',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Kauffman Stadium',
+    'lat': 39.051389,
+    'lng': -94.480556,
+    'teams': [{ 'name': 'Kansas City Royals',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Progressive Field',
+    'lat': 41.495833,
+    'lng': -81.685278,
+    'teams': [{ 'name': 'Cleveland Indians',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'U.S. Cellular Field',
+    'lat': 41.83,
+    'lng': -87.633889,
+    'teams': [{ 'name': 'Chicago White Sox',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Angel Stadium of Anaheim',
+    'lat': 33.800278,
+    'lng': -117.882778,
+    'teams': [{ 'name': 'Los Angeles Angels of Anaheim',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Global Life Park in Arlington',
+    'lat': 32.751389,
+    'lng': -97.082778,
+    'teams': [{ 'name': 'Texas Rangers',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Minute Maid Park',
+    'lat': 29.756944,
+    'lng': -95.355556,
+    'teams': [{ 'name': 'Houston Astros',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Safeco Field',
+    'lat': 47.591389,
+    'lng': -122.3325,
+    'teams': [{ 'name': 'Seattle Mariners',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Citi Field',
+    'lat': 40.756944,
+    'lng': -73.845833,
+    'teams': [{ 'name': 'New York Mets',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Citizens Bank Park',
+    'lat': 39.905833,
+    'lng': -75.166389,
+    'teams': [{ 'name': 'Philadelphia Phillies',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Marlins Park',
+    'lat': 25.778056,
+    'lng': -80.219722,
+    'teams': [{ 'name': 'Miami Marlins',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Nationals Park',
+    'lat': 38.872778,
+    'lng': -77.0075,
+    'teams': [{ 'name': 'Washington Nationals',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Busch Stadium',
+    'lat': 38.6225,
+    'lng': -90.193056,
+    'teams': [{ 'name': 'St. Louis Cardinals',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Great American Ball Park',
+    'lat': 39.0975,
+    'lng': -84.506667,
+    'teams': [{ 'name': 'Cincinnati Reds',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Miller Park',
+    'lat': 43.028333,
+    'lng': -87.971111,
+    'teams': [{ 'name': 'Milwaukee Brewers',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'PNC Park',
+    'lat': 40.446944,
+    'lng': -80.005833,
+    'teams': [{ 'name': 'Pittsburgh Pirates',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Wrigley Field',
+    'lat': 41.948333,
+    'lng': -87.655556,
+    'teams': [{ 'name': 'Chicago Cubs',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'AT&T Park',
+    'lat': 37.778611,
+    'lng': -122.389167,
+    'teams': [{ 'name': 'San Francisco Giants',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Chase Field',
+    'lat': 33.445278,
+    'lng': -112.066944,
+    'teams': [{ 'name': 'Arizona Diamondbacks',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Coors Field',
+    'lat': 39.756111,
+    'lng': -104.994167,
+    'teams': [{ 'name': 'Colorado Rockies',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Dodger Stadium',
+    'lat': 34.073611,
+    'lng': -118.24,
+    'teams': [{ 'name': 'Los Angeles Dodgers',
+                'league': 'MLB' }]
+  },
+  {
+    'name': 'Petco Park',
+    'lat': 32.7073,
+    'lng': -117.1566,
+    'teams': [{ 'name': 'San Diego Padres',
+                'league': 'MLB' }]
   }
 ]; // stadiumData
